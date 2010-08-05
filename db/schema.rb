8ab22991fe2f234a55c1ad9dd15ddb8078cf3d24
Joni Lahtinen <joni.lahtinen@gmail.com>
2010-08-05 22:57:43 +0300

    added description to project

--- db/schema.rb
+++ db/schema.rb
@@ -9,12 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100805163524) do
+ActiveRecord::Schema.define(:version => 20100805192019) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "tasks", :force => true do |t|
