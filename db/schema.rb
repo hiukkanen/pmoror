106e0793fddbe739a4a66c75e74f2279371b3425
Joni Lahtinen <joni.lahtinen@gmail.com>
2010-08-05 19:36:29 +0300

    migration -> project_id field to task

--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100804181617) do
+ActiveRecord::Schema.define(:version => 20100805163524) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
     t.integer  "status"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "project_id"
   end
 
 end
