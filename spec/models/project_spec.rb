0a0f296334062778b81603524082afd031540b64
Joni Lahtinen <joni.lahtinen@gmail.com>
2010-08-05 20:35:24 +0300

    created some specs

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,13 +1,8 @@
 require 'spec_helper'
 
 describe Project do
-  before(:each) do
-    @valid_attributes = {
-      :name => "value for name"
-    }
-  end
 
   it "should create a new instance given valid attributes" do
-    Project.create!(@valid_attributes)
+    Factory.create :project
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,14 +1,8 @@
 require 'spec_helper'
 
 describe Task do
-  before(:each) do
-    @valid_attributes = {
-      :name => "value for name",
-      :status => 1
-    }
-  end
 
   it "should create a new instance given valid attributes" do
-    Task.create!(@valid_attributes)
+    Factory.create :task
   end
 end
