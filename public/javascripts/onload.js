6d8c963249f374cdac662b465ff610ef9e4c3703
Joni Lahtinen <joni.lahtinen@gmail.com>
2010-08-15 12:07:45 +0300

    ajax to get next status for project

--- public/javascripts/onload.js
+++ public/javascripts/onload.js
@@ -1,7 +1,18 @@
 function sortable_project_table(){
   $("#projects").tablesorter();
-  console.log($("#projects"));
+}
+function clickable_tasks(){
+  $("td.task").click(function() {
+      var id = $(this).attr("data-id");
+      var background = $.ajax({
+        type: "GET",
+        url: "/tasks/next?id="+id,
+        async: false
+      }).responseText;
+      $(this).css("background-color", "red");
+  });
 }
 $(document).ready(function() {
     sortable_project_table();
+    clickable_tasks();
 });
