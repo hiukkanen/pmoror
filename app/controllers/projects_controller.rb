c8b87e55185d852a5acf97fe0d9311a17c374fb1
Joni Lahtinen <joni.lahtinen@gmail.com>
2010-08-15 11:31:34 +0300

    added create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,15 @@ class ProjectsController < ApplicationController
 
   def index
     @projects = Project.all
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new params[:project]
+    if @project.save
+      @project = Project.new
+    end
+    redirect_to projects_path   
   end
 
 end
