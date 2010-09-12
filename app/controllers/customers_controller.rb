79ef1196e82989f98a70396d263480d7c56cdc1b
Joni Lahtinen <joni.lahtinen@gmail.com>
2010-09-12 17:26:07 +0300

    It is now possible to edit customer name with jeditable

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,10 @@
 class CustomersController < ApplicationController
+
+  def name
+    customer = Customer.find params[:id]
+    customer.name = params[:value]
+    customer.save!
+    render :text => customer.name
+  end
+
 end
