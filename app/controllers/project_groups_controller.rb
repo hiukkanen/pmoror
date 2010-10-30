4a0456768fc4f59e813d7d23e068bfcc483c21bc
Joni Lahtinen <joni.lahtinen@gmail.com>
2010-10-30 19:48:55 +0300

    New and create actions

--- app/controllers/project_groups_controller.rb
+++ app/controllers/project_groups_controller.rb
@@ -2,5 +2,12 @@ class ProjectGroupsController < ApplicationController
 
   def new
     @project_group = ProjectGroup.new
+    id = params['id']
+    id.to_i.times { @project_group.tasks.build }
+  end
+
+  def create
+    @project_group = ProjectGroup.new params[:project_group]
+    @project_group.save ? redirect_to(project_group_path(@project_group)) : render(:action => :new)
   end
 end
