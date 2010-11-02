909df1a2131beffa9cb4a8dea9c27a6ffeafae55
Joni Lahtinen <joni.lahtinen@gmail.com>
2010-11-02 17:04:29 +0200

    Default scope order

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,8 @@ class Task < ActiveRecord::Base
   validates_presence_of :name
   belongs_to :project
   has_one :status
+  has_one :task
+  default_scope :order => 'id'
 
   def status!
     self.status = Status.create! :task => self unless self.status != nil
