27bc0576045bc0267c41895c06fd6513e18b3dfc
Joni Lahtinen <joni.lahtinen@gmail.com>
2010-11-02 16:24:30 +0200

    Edit, index and update

--- app/controllers/project_groups_controller.rb
+++ app/controllers/project_groups_controller.rb
@@ -6,8 +6,23 @@ class ProjectGroupsController < ApplicationController
     id.to_i.times { @project_group.tasks.build }
   end
 
+  def edit
+    @project_group = ProjectGroup.find params[:id]
+  end
+
+  def index
+    @project_groups = ProjectGroup.all
+  end
+
   def create
     @project_group = ProjectGroup.new params[:project_group]
     @project_group.save ? redirect_to(project_group_path(@project_group)) : render(:action => :new)
   end
+
+  def update
+    project_group = ProjectGroup.find params[:id]
+    project_group.update_attributes params[:project_group]
+    redirect_to project_groups_path
+  end
+
 end
