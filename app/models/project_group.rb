0fff0b627e8e5c5ed648cc187c520cd41934b6b3
Joni Lahtinen <joni.lahtinen@gmail.com>
2010-11-02 10:37:59 +0200

    new_project method that returns project assosiated for this group

--- app/models/project_group.rb
+++ app/models/project_group.rb
@@ -2,4 +2,11 @@ class ProjectGroup < ActiveRecord::Base
   has_many :projects
   has_many :tasks
   accepts_nested_attributes_for :tasks
+
+  def new_project
+    project = Project.new
+    project.project_group = self
+    project.build_customer
+    return project
+  end
 end
