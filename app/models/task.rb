a53e92966741ebf25bee74504c76feeb24f9432f
Joni Lahtinen <joni.lahtinen@gmail.com>
2010-11-02 18:17:39 +0200

    Has_many and belongs_to are now fixed

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,8 +2,21 @@ class Task < ActiveRecord::Base
   validates_presence_of :name
   belongs_to :project
   has_one :status
-  has_one :task
+  has_many :tasks
+  belongs_to :task
+  belongs_to :project_group
   default_scope :order => 'id'
+
+  before_save :validate
+  
+  def validate
+    if project != nil
+      raise "parent task is missing" if task == nil
+    end
+    if project_group != nil
+      raise "should not have parent" if task != nil
+    end
+  end
 
   def status!
     self.status = Status.create! :task => self unless self.status != nil
